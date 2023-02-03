d8622ba7cac6b70d9fcef20c96405a362cff3ec6
Poyo2007 <82355015+Poyo2007@users.noreply.github.com>
2023-02-02 21:12:02 -0500

    update source/Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -86,6 +86,8 @@ class Main extends Sprite
 
 		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
 
+		SUtil.checkFiles();
+
 		#if !mobile
 		fpsCounter = new FPS(10, 3, 0xFFFFFF);
 		addChild(fpsCounter);
